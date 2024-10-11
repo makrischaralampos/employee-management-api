2e450c1572274137b130c0832c21a16e548c0bbe
makris charalampos <mcshooter23@gmail.com>
2024-10-11 13:36:40 +0300

    Use Hibernate to map the Employee model to H2

--- src/main/java/com/example/employee_management_api/model/Employee.java
+++ src/main/java/com/example/employee_management_api/model/Employee.java
@@ -1,14 +1,12 @@
 package com.example.employee_management_api.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "employees")
 @Data // Lombok annotation to generate getters, setters, toString, equals, and hashCode methods
 @NoArgsConstructor // Lombok annotation to generate a no-arguments constructor
 @AllArgsConstructor // Lombok annotation to generate an all-arguments constructor
@@ -18,8 +16,15 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @Column(name = "department")
     private String department;
 }
