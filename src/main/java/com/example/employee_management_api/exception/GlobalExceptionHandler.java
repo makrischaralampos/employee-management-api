101a10fa12f972cb33c87dad505eefad357f2055
makris charalampos <mcshooter23@gmail.com>
2024-10-13 13:02:19 +0300

    Optimize Hibernate queries for caching

--- src/main/java/com/example/employee_management_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/employee_management_api/exception/GlobalExceptionHandler.java
@@ -4,7 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,9 +31,11 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handleGeneralExceptions(Exception ex) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Map<String, String>> handleGeneralExceptions(Exception ex, WebRequest request) {
         Map<String, String> errorResponse = new HashMap<>();
-        errorResponse.put("error", "Internal server error");
+        errorResponse.put("error", "An unexpected error occurred.");
+        errorResponse.put("message", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/example/employee_management_api/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee_management_api/repository/EmployeeRepository.java
@@ -1,14 +1,18 @@
 package com.example.employee_management_api.repository;
 
 import com.example.employee_management_api.model.Employee;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    @QueryHints({@QueryHint(name = "org.hibernate.cacheable", value = "true")})
     List<Employee> findByDepartment(String department);
+
     List<Employee> findByEmail(String email);
     List<Employee> findByDepartmentAndEmail(String department, String email);
 }
